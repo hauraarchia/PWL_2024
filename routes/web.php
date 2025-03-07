e486d261b29686e0ab788785a36ab3909141205e
haura archia sarapova putri kresna <hauraarchia@gmail.com>
2025-03-07 22:36:27 +0700

    jobsheet1-praktikum1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,46 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//js 2 p1
+//basic route
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2341760006 - Haura Archia';
+});
+
+//route parameters
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya ' .$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Post ke-'.$postId." Komentar ke: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($articleId) {
+    return 'Halaman Artikel dengan ID '.$articleId;
+});
+
+
+// optional parameters
+// Route::get('/user/{name?}', function ($name = null) {
+//     return 'Nama Saya ' . $name;
+// });
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama Saya ' . $name;
+});
+
